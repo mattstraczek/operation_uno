8f51f173ba3990a0381d7b8a54e462af0c1d0f39
mattstraczek <mstraczek10@gmail.com>
2022-02-21 00:23:51 -0600

    Added home screen and button class

--- Utilities.py
+++
@@ -1,8 +0,0 @@
-# functions -------------------------
-
-def redrawWindow(window):
-    window.fill(background_color) # change color if you want
-
-def displayMessage(msg, color, dest, display):
-    screen_text = font.render(msg, True, color)
-    display.blit(screen_text, dest)
