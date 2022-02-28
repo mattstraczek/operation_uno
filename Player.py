a407f158f8e91a9a3421a05a5ea5a784139eb2a1
anoorullah <82806112+anoorullah@users.noreply.github.com>
2022-02-27 18:45:09 -0600

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,30 +1,9 @@
 import Deck
 
+
 class Player():
     def __init__(self):
         self.hand = []
     
     def draw(self):
         self.hand.append(Deck.draw())
-        
-class Player():
-    def __init__(self, name):
-        self.name = str(name)
-        print("Creating", name)
-        self.hand = []
-    
-    def addCard(self, card):
-        print("Adding", card, "to", self.name, "'s hand")
-        self.hand.append(card)
-
-    def displayHand(self):
-        handCards = ""
-        for card in self.hand:
-            handCards += (card.cardInfo() + ", ")
-        return handCards
-
-    def __str__(self):
-        return str(self.name) + "'s hand: " + str(self.displayHand())
-       
-    def playCard():
-        return
