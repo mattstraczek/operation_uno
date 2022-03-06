f74f0a67118ef1886cc3fad274bbb5f2c9be933a
anoorullah <82806112+anoorullah@users.noreply.github.com>
2022-03-06 12:51:41 -0600

    Created Separate File for Changing Difficulty

---
+++ Difficulty.py
@@ -0,0 +1,11 @@
+#from Ruleset import Ruleset
+from Deck import Deck
+from Player import Player
+from AI import AI 
+from Button import Button
+from Game import Game
+import pygame
+
+def Difficulty:
+    # This will let user change difficulty mode in pop-up menu screen
+    
