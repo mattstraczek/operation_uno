491bc3ec16846c41f879b8846d82dc5085ec0133
anoorullah <82806112+anoorullah@users.noreply.github.com>
2022-03-06 13:17:24 -0600

    Completed Difficulty.py

--- Difficulty.py
+++ Difficulty.py
@@ -9,4 +9,84 @@ import pygame
 class Difficulty:
     # This will let user change difficulty mode in pop-up menu screen
     def changeDifficulty():
-        
+        # changeable constants -----
+        screen_x = 800
+        screen_y = 600
+        background_size = (screen_x, screen_y)
+
+        colors = {
+            "white": (255, 255, 255),
+            "grey": (230, 230, 230),
+            "green": (0,255,0),
+            "blue": (0,0,255),
+            "red": (255,0,0),
+            "dark_red": (139,0,0),
+            "crimson": (246,26,26),
+            "black": (0,0,0),
+            "purple": (48,25,52),
+            "yellow": (255,255,0)
+        }
+
+        background_color = colors["red"]
+
+        # functions -------------------------
+
+        def displayWindow(window):
+            window.fill(background_color) # change color if you want
+
+        def displayMessage(msg, color, dest, display):
+            screen_text = font.render(msg, True, color)
+            display.blit(screen_text, dest)
+
+        # window set up
+        pygame.init()
+        window = pygame.display.set_mode(background_size)
+        pygame.display.set_caption('Operation-Uno')
+
+        # text and font
+        font = pygame.font.Font('Resources/Font/OpenSans-ExtraBold.ttf', 60)
+        button_font = pygame.font.Font('Resources/Font/OpenSans-Regular.ttf', 22)
+        png = pygame.image.load('Resources/Images/uno.png')
+
+        easy_button = Button(colors["green"], [100, 125], [100, 50], button_font, "Easy", colors["green"], (37,186,176))
+        medium_button = Button(colors["yellow"], [100, 200], [200, 50], button_font, "Medium", colors["yellow"], (37,186,176))
+        hard_button = Button(colors["purple"], [100, 275], [200, 50], button_font, "Hard", colors["purple"], (37,186,176))
+
+        run = True
+        while run:
+            mouse_pos = pygame.mouse.get_pos()
+
+            # Have the background fanfare playing while the menu is running
+            msound.load("Resources/Sounds/Fanfare-sound.mp3")
+            msound.play(-1)
+
+            # ---------------- updates -----------------
+            easy_button.updateButton(mouse_pos, window)
+            medium_button.updateButton(mouse_pos, window)
+            hard_button.updateButton(mouse_pos, window)
+
+            for event in pygame.event.get(): 
+                # ----------- ongoing checks (controls, updates, etc) ----------
+                
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if easy_button.isHovered():
+                        print('Pressed Easy Button')
+                    if medium_button.isHovered():
+                        print('Pressed Medium Button')
+                    if hard_button.isHovered():
+                        print('Pressed Hard Button')
+            
+            # ---------- renders --------------
+            displayWindow(window)
+            displayMessage("OPERATION UNO", colors["white"], [150, 25], window)
+            easy_button.displayButton(window)
+            medium_button.displayButton(window)
+            hard_button.displayButton(window)
+            display.blit(png, (500, 300))
+            # pygame.display.flip() # Do we need this? I feel like this will change the layout of the window
+
+            # ----------- final update --------------
+            pygame.display.update()
