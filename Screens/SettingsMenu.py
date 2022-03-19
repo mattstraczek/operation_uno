a9174b1ed2b4b1ecd8e8d7a76158ebb3ca525899
anoorullah <82806112+anoorullah@users.noreply.github.com>
2022-03-19 16:41:19 -0500

    Changed screen color to purple

--- Screens/SettingsMenu.py
+++ Screens/SettingsMenu.py
@@ -3,8 +3,8 @@ from Components import Button, Message
 from Screens import MainMenu
 
 class Settings():
-    def __init__(self, width=800, height=600, bg_color=pygame.Color("Black")):
-        """ Initializes the Main Menu with default size of 800x600 and a black background """
+    def __init__(self, width=800, height=600, bg_color=pygame.Color("Purple")):
+        """ Initializes the Main Menu with default size of 800x600 and a Purple background """
         self.title = "Settings"
         self.w = width
         self.h = height
